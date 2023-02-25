27f4c26fda336ddb9b0a132de2a69535401a014a
Konstantin Chern <codahl@yandex.ru>
2023-02-25 22:49:04 +0300

    Task1 solved

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,57 @@
 
 // [345, 897, 568, 234] -> 2
 
+
+Console.Clear();
+
+
+int HonestArray(int[] array)
+{
+    int sum = 0;  // Сумма накопления
+    for (int i = 0; i < array.Length; i++) // цикл перебора по массиву
+    {
+
+        if (array[i] % 2 == 0)
+        {
+            sum++;             // накапливаем сумму
+        }
+
+
+    }
+    return sum;
+}
+
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+
+int size = 12;
+int minValue = 100;
+int maxValue = 1000;
+
+int[] myArray = GenerateArray(size, minValue, maxValue);
+PrintArray(myArray);
+int Honest = HonestArray(myArray);
+System.Console.WriteLine( $"сумма четных элементов массива = {Honest} ");
+
+
+
+
